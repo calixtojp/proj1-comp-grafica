cca2f8e5d9482ce17d938e554eea169ed75efd52
sb-lucas <lucas_b.480@hotmail.com>
2024-09-14 22:43:53 -0300

    so algumas correcoes

--- chao.py
+++ chao.py
@@ -11,10 +11,11 @@ class Chao:
     self.vertices = np.zeros(self.tam, [("position", np.float32, 3)])
 
     n_gramas = int((self.tam - 4)/4)
-
     grama = []
     x = -1
     for i in range(n_gramas):
+    # preenchendo as coordenadas de cada vértice 
+    # automaticamente e acordo com o padrao da grama
       x += 0.03
       grama.append((x,        +0.35, 0))
       grama.append((x + 0.01, +0.35, 0))
@@ -22,15 +23,14 @@ class Chao:
       grama.append((x + 0.01, +0.55, 0))
 
     # preenchendo as coordenadas de cada vértice
-    chao = [
-      #Chao em si
+    terra = [
       (-1, -0.5, 0),
       (+1, -0.5, 0),
       (-1, +0.5, 0),
       (+1, +0.5, 0),
     ]
 
-    self.vertices['position'] = np.array(grama + chao, np.float32)
+    self.vertices['position'] = np.array(grama + terra, np.float32)
     self.r_len = len(self.vertices['position'])
 
 
@@ -43,23 +43,17 @@ class Chao:
 
   def desenhar(self, program, loc_color, pos):
         
-    mat_rotation_x = get_matriz_rotacao_x(0)
-    mat_rotation_y = get_matriz_rotacao_y(0)
-    mat_rotation_z = get_matriz_rotacao_z(0)
     mat_translacao = get_matriz_translacao(0, -0.5, 0)
     
-
-    # sequencia de transformações: rotação z, rotação y, rotação x, translação
-    mat_transform = self.multiplica_matriz(mat_rotation_z,mat_rotation_y)
-    mat_transform = self.multiplica_matriz(mat_rotation_x,mat_transform)
-    mat_transform = self.multiplica_matriz(mat_translacao,mat_transform) 
+    mat_transform = mat_translacao
 
     loc = glGetUniformLocation(program, "mat_transformation")
     glUniformMatrix4fv(loc, 1, GL_TRUE, mat_transform)
     
-    
+    #Grama    
     glUniform4f(loc_color, 0, 1, 0, 1)  
     glDrawArrays(GL_TRIANGLE_STRIP, pos, self.tam-4)
 
+    #Terra
     glUniform4f(loc_color, 0.49, 0.33, 0.10, 1)  
     glDrawArrays(GL_TRIANGLE_STRIP, pos+self.tam-4, 4)
